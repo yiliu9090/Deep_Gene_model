0fb66e326dbe7ee60dcdd01f2d28809d68f7b882
YI LIU <Yi.liu9090@gmail.com>
2020-08-03 15:19:16 -0500

    Improve on TF_PWM
    Make an improvement on TF_PWM

--- Deep_Gene/TF_PWM.py
+++ Deep_Gene/TF_PWM.py
@@ -9,96 +9,105 @@ from tensorflow.keras.initializers import RandomUniform,Constant
 from tensorflow.keras import regularizers
 from tensorflow.keras import backend as K
 
-class protein:
+class TF_PWM:
     '''
-    A protein includes a PWM
+    The true purpose of protein class is to deal with all the calculations concerning PWMs. Thats it!
     
-    The protein class should be able to all the log frequency computed, the max value
+    Calculation of PWM: 
+        1. Relative PWM with backgound frequency
+        2. Reverse Compliment Build
+        3. Footprint padding
+        4. Max constant compute
+        5. tf.tensor build 
     
-    This helps the user to input protein as 
     '''
-    def __init__(self,name = 'Nothing',PWM = np.array([0.25,0.25,0.25,0.25]),\
-                 background_frequency = np.array([0.25,0.25,0.25,0.25]),footprint = 1):
+    def __init__(self,name = 'Nothing',PWM = np.array([[0.25,0.25,0.25,0.25]]),\
+                 background_frequency = np.array([0.25,0.25,0.25,0.25]), footprint_adjust = [0,0]):
         
-        self.name = name
+        '''
+        @param name: a str of 
+        @param PWM: a np.array([A,C,G,T])
+        @param background_frequency: a np.array([A,C,G,T])
+        @param footprint_adjust: List of 2 numbers. 
+        '''
         
-        self.PWM = PWM
+        self.name = name #name of the TF
         
-        self.background_frequency = background_frequency
+        self.PWM = PWM #PWM matrix
         
-        self.footprint = len(PWM)
+        self.background_frequency = background_frequency #background Frequency of the organism
 
-        self.biological_foot_print = footprint #biological footprint is different from the actual footprint
+        self.biological_foot_print = len(self.PWM) #biological footprint is different from the actual footprint
+        
+        self.footprint_adjust = footprint_adjust
+        
+        self.footprint = PWM.shape[0] + self.footprint_adjust[0] + self.footprint_adjust[1]
         
         self.log_frequency_f ,self.log_frequency_r = self.log_frequency_compute()
         
         self.max_log_frequency = self.max_log_frequency_compute()
 
     
-    def log_frequency_compute(self,background = True):
-
-        y = self.PWM
-
-        z = np.zeros((len(y),4))
-
-        c = 0
+    def log_frequency_compute(self,add_background = True):
         
-        for x in y:
+        '''
+        From PWM data table to the actual PWM matrix for computation requires many crucial steps that includes a 
+        
+        an adding operations followed by a sum operation
+        
+        '''
 
-            x = x + self.background_frequency
+        log_PWM_matrix = np.zeros((self.footprint,4))
 
-            z[c] = np.log(np.divide(x/np.sum(x),self.background_frequency))
+        reverse_complement = np.zeros((self.footprint,4))
+        
+        reverse_complement_adjust =  self.footprint_adjust[0] - self.footprint_adjust[1]
+        for i in range(len(self.PWM)):
 
-            c = c + 1
+            PWM_one_column = self.PWM[i] + self.background_frequency
 
-        zr = np.copy(z[::-1])
-
-        for i in range(len(zr)):
+            log_PWM_matrix[self.footprint_adjust[0] + i] = np.log(np.divide(PWM_one_column/np.sum(PWM_one_column),\
+                                                                            self.background_frequency))
             
-            zr[i][0], zr[i][1],zr[i][2],zr[i][3]=zr[i][3], zr[i][2],zr[i][1],zr[i][0]
-        
-        return z , zr
-    
-    def max_log_frequency_compute(self,backgound = True):
-        y =  self.PWM
-
-        z = 0
-        
-        for x in y:
-        
-            x = x + self.background_frequency
+            #ACGT A==T, C==G, ACGT
             
-            z = z + np.max(np.log(np.divide(x/np.sum(x),self.background_frequency)))
-
-        return(z)
-
-    def block_generate(self, DNA_input, concentration_input,score_cut =0,adjustment =0):
+            reverse_complement[self.footprint -reverse_complement_adjust -i-1] =\
+            self.reverse_complement_op(log_PWM_matrix[self.footprint_adjust[0] + i])
         
-        '''
-        Protein block generation essentially allow me to generate blocks of data 
-
-        protein
-        '''
-
-        PWMf = tf.expand_dims(tf.expand_dims( tf.convert_to_tensor(value=self.log_frequency_f, dtype='float32', name='PWM_'+self.name +'_f'),-1),-1)
-        
-        PWMr = tf.expand_dims(tf.expand_dims( tf.convert_to_tensor(value=self.log_frequency_f, dtype='float32', name='PWM_'+self.name +'_r'),-1),-1)
-        
-        return mc.DNA_protein_block( PWMf , PWMr ,self.max_log_frequency, \
-                 self.footprint,  concen_input = concentration_input,\
-                 DNA = DNA_input , score_cut = score_cut,\
-                 adjustment = adjustment)
+        return log_PWM_matrix , reverse_complement
     
-    def block_generate_loose(self, DNA_input, concentration_input,score_cut =0,adjustment =0):
-
+    def reverse_complement_op(self,DNA):
         '''
-        block_generate_loose does the same thing as block generate but loose up some approximations
+        return the reverse complement so that the code is 
         '''
-        PWMf = tf.expand_dims(tf.expand_dims( tf.convert_to_tensor(value=self.log_frequency_f, dtype='float32', name='PWM_'+self.name +'_f'),-1),-1)
+        return np.array([DNA[3], DNA[2], DNA[1], DNA[0]])
+    
+    def max_log_frequency_compute(self,add_backgound = True):
+        
+        '''
+        return the maximum log_value
+        '''
+        max_log_value = 0
+        
+        for PWM_i in self.PWM:
+        
+            PWM_one_column = PWM_i + self.background_frequency
+            
+            max_log_value = max_log_value + np.max(np.log(np.divide(PWM_one_column/np.sum(PWM_one_column),\
+                                                                    self.background_frequency)))
+        return(max_log_value)
 
-        PWMr = tf.expand_dims(tf.expand_dims( tf.convert_to_tensor(value=self.log_frequency_f, dtype='float32', name='PWM_'+self.name +'_r'),-1),-1)
+    def PWM_tensor_generate(self, dtypes = 'float32'):
+        
+        '''
+        Protein block generation generates 
+        '''
+
+        PWMf = tf.expand_dims(tf.expand_dims( tf.convert_to_tensor(value=self.log_frequency_f, \
+                                                                   dtype=dtypes, name='PWM_'+self.name +'_f'),-1),-1)
+        
+        PWMr = tf.expand_dims(tf.expand_dims( tf.convert_to_tensor(value=self.log_frequency_f, dtype=dtypes, \
+                                                                   name='PWM_'+self.name +'_r'),-1),-1)
+        
                 
-        return mc.DNA_protein_block_loose( PWMf , PWMr ,self.max_log_frequency, \
-                 self.footprint,  concen_input = concentration_input,\
-                 DNA = DNA_input , score_cut = score_cut,\
-                 adjustment = adjustment)
+        return PWMf, PWMr
