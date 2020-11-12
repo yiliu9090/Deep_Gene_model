76489d1eb62d9bf28ada972563021db41b9371b5
YI LIU <Yi.liu9090@gmail.com>
2020-11-11 19:36:18 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Deep_Gene-yi_liu", # Replace with your own username
+    name="DeepGene", # Replace with your own username
     version="0.1.1",
     author="Yi Liu",
     author_email="yil@uchicago.edu",
